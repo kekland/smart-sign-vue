78e75bcdc6958094700121fa5bb01be644ef0d4e
kekland <kk.erzhan@gmail.com>
2018-08-20 18:13:37 +0600

    Copied all HTML/CSS code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-md-9' id='map-block'>
+                    <div id='map' style='width: 100%; height: 100vh'></div>
+                </div>
+                <div class='col-md-3' id='info-block'>
+                    <h3>Info</h3>
+                    <hr>
+                </div>
+            </div>
+        </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-  components: {
-  },
+  components: {},
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#map-block {
+  padding-left: 0;
+  padding-right: 0;
+}
+
+#info-block {
+  box-shadow: 0 0px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  background-color: #fff;
+
+  padding: 16px;
+}
+
+#info-block:hover {
+  box-shadow: 0 0px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
 </style>
